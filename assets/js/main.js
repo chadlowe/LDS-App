dd93ae8e59963e8e733e1e6433fd6f3929f38347
Chad Lowe <chad.lowe@ldssystems.co.uk>
2011-10-04 15:59:59 +0100

    Signed-off-by: Chad Lowe <chad.lowe@ldssystems.co.uk>

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,23 @@ function geo(){
   };
   navigator.geolocation.getCurrentPosition(suc, locFail);
 }
-function vibratephone(){
-  navigator.notification.vibrate(0);
+
+function clockin(){
+  var suc = function(p) {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var time = '';
+    if(hours > 11){
+      time = hours + ':' + minutes +' PM'
+    } else {
+      time = hours + ':' + minutes +' PM'
+    }
+    
+    document.getElementById("ClockedIn").innerHTML =  'You have clocked in at: <br>' + time + "<br><br>Your location:<br>" + p.coords.latitude + "<br>" + p.coords.longitude;
+  };
+  var locFail = function() {
+    document.getElementById("ClockedIn").innerHTML = "So sorry but your gps isnt on";
+  };
+  navigator.geolocation.getCurrentPosition(suc, locFail);
 }
